d9ecc7a0bc13a03861b84e6e5c7aac91fcd2dfb8
George <georgejweiler@gmail.com>
2016-07-27 15:57:09 -0700

    chore[cleanup] remove comment and fix indents

--- server/server.js
+++ server/server.js
@@ -7,34 +7,25 @@ const express = require('express'),
   http = require('http').Server(app),
   request = require('request'),
   bodyParser = require('body-parser'),
-  path = require('path');
+  path = require('path'),
+  config = require('./../webpack.dev.config'),
+  compiler = webpack(config);
 
-  app.use(express.static(__dirname + '/../dist'));
-  app.use(bodyParser.json());
-
-
-// app.get('/', function(req, res){
-//  res.sendFile(__dirname +'/../dist/index.html');
-//  console.log("connecting to root...");
-// });
-
-const config = require('./../webpack.dev.config');
-const compiler = webpack(config);
-
+app.use(express.static(__dirname + '/../dist'));
+app.use(bodyParser.json());
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
-  stats: {colors: true}
+  stats: {
+    colors: true
+  }
 }))
-
+app.use(router);
 app.use(webpackHotMiddleware(compiler, {log: console.log}));
-
 
 router.get('/',function(req, res){
   res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
   console.log("connecting to root...");
 });
-
-app.use(router);
 
 http.listen(process.env.PORT || 8000, function(){
   console.log('App listening on port 8000');
