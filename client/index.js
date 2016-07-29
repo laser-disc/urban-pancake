917ad44c2eb2189529d9b5f2b7c79ea0148482e6
Blake Contreras <shardsofsilver@gmail.com>
2016-07-29 16:54:04 -0700

    chore[server] Add functionality and npm scripts to launch in dev or prod env.
    npm run dev -- Launch node server with hot reloading for development.
    npm start -- Run webpack and launch node server for production environment.
    npm run webdev -- Launch webpack-dev-server (you shouldn't need this).

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
-import HelloWorld from './components/helloworld';
+// App entry point.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise';
-
 import App from './components/app';
 import reducers from './reducers';
 
@@ -16,7 +16,7 @@ ReactDOM.render(
   </Provider>
   , document.querySelector('#app'));
 
-// If in dev environment, app must accept hot reloading
+// The entry point of our app must accept hot reloading in dev environment.
 if (module.hot) {
   module.hot.accept();
 }
