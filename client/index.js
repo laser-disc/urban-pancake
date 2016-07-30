909ae2e6b24718b83e4866197e8cf91688e6bd24
Reed Cureton <emailreedc@gmail.com>
2016-07-29 19:40:31 -0700

    Fix[client] bug fix on TweetItem and index.js

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise';
 import App from './components/app';
-import reducers from './reducers';
+import rootReducer from './reducers/reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
