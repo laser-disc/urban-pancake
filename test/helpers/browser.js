7938e3a2c2a18d169afd5c3b37513429188054bb
Blake Contreras <shardsofsilver@gmail.com>
2016-07-29 19:18:51 -0700

    test[init] Add helper to set up mock DOM for client tests.

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -1,20 +1,6 @@
 require('babel-register')();
 
 const jsdom = require('jsdom').jsdom;
-
-let exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
+global.document = jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
-
-global.navigator = {
-  userAgent: 'node.js'
-};
-
-documentRef = document;
+global.navigator = global.window.navigator;
