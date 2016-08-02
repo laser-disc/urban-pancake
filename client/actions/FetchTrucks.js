f5f750501414ad747a3b8640a5808028fbe5522a
bterrot <torre.bonnie@gmail.com>
2016-08-02 07:42:14 -0700

    docs[client] Annotate updated React/Redux files

--- client/actions/FetchTrucks.js
+++ client/actions/FetchTrucks.js
@@ -1,20 +1,14 @@
+// FetchTrucks: makes a call to the database in order to update the state. It's called in the container TruckList.js
+
 import axios from 'axios';
-// import API_KEY from './../env/config';
-
-
-// const ROOT_URL = ``;
 
 export const FETCH_TRUCKS = 'FETCH_TRUCKS';
 
 export function FetchTrucks() {
   const url = '/API/fetchAll';
   const request = axios.get(url)
-  // .then(function (response) {
-  //   console.log('INSIDE PROMISE OF ACTION', response);
-  // })
   return {
     type: FETCH_TRUCKS,
     payload: request
   };
-
-}
+};
--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -1,8 +1,9 @@
+// TruckItem: the individual tweets that will appear on the page
+
 import React, {Component} from 'react';
 
 export default class TruckItem extends Component {
   render() {
-    // console.log('INSIDE TRUCKITEM', this.props)
     return (
       <div>
         <div>Your Truck: {this.props.truck.handle}</div>
