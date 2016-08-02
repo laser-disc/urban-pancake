7941c6ca870d05221e60d6175ab36fdaaf62c296
bterrot <torre.bonnie@gmail.com>
2016-08-02 14:26:30 -0700

    fix[client/components] update TruckItem to include all database schema

--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -7,13 +7,13 @@ import React, {Component} from 'react';
 export default class TruckItem extends Component {
   render() {
     return (
-      <div>
-        <div>Your Truck: {this.props.truck.name}</div>
-        <div>Your Truck: {this.props.truck.handle}</div>
-        <div>Your Truck: {this.props.truck.description}</div>
-        <div>Last Tweet: {this.props.truck.message}</div>
-        <div>Time: {this.props.truck.timeStamp}</div>
-        <img src={this.props.truck.image} />
+      <div className='TruckItem'>Your Truck:
+        <div className='TruckName'>{this.props.truck.name}</div>
+        <div className='TruckHandle'>{this.props.truck.handle}</div>
+        <div className='TruckDescription'>{this.props.truck.description}</div>
+        <div className='TruckTweet'>Last Tweet: {this.props.truck.message}</div>
+        <div className='TruckTimeStamp'>Time: {this.props.truck.timeStamp}</div>
+        <img className='TruckAvatar' src={this.props.truck.image} />
       </div>
     );
   };
