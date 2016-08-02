6a1f6c9992b195bb8935eea1fdd4be18e5661fce
bterrot <torre.bonnie@gmail.com>
2016-08-02 13:48:29 -0700

    fix[client/components/TruckItem] update TruckItem to include all schema from the database

--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -8,7 +8,9 @@ export default class TruckItem extends Component {
   render() {
     return (
       <div>
+        <div>Your Truck: {this.props.truck.name}</div>
         <div>Your Truck: {this.props.truck.handle}</div>
+        <div>Your Truck: {this.props.truck.description}</div>
         <div>Last Tweet: {this.props.truck.message}</div>
         <div>Time: {this.props.truck.timeStamp}</div>
         <img src={this.props.truck.image} />
