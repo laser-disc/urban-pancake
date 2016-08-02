13413a18406c8844b6f5300b0b0994cbc22f7c27
Reed Cureton <emailreedc@gmail.com>
2016-08-02 15:27:36 -0700

    Fix [deployment] API keys set to heroku environment variables and to env/config file. Runs on localhost and heroku

--- db/config.js
+++ db/config.js
@@ -1,10 +1,13 @@
-// const secretKeys = require('../env/config');
+'use strict'
+let secretKeys = null;
+if(!process.env['MONGOOSE_URI']) {
+  secretKeys = require('../env/config');
+}
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env['MONGOOSE_URI'] || secretKeys.MONGOOSE_URI);
 
 const db = mongoose.connection;
-// db.on('error', console.log('There was an error connecting to mongoose'));
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('in DB');
