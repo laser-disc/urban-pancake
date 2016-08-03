94db88e809100d67dbd5712cfe830524c3a564bc
George <georgejweiler@gmail.com>
2016-08-02 19:10:37 -0700

    refactor[client] fix single quotes

--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -7,16 +7,16 @@ import React, {Component} from 'react';
     
     render() {
       return (
-      <div className='TruckItem'>
+      <div className="TruckItem">
           <div className="tweet col-lg-offset-3 col-sm-6 col-md-6 col-lg-6">
-              <img className='TruckAvatar' src={this.props.truck.image} alt="Truck Image" />
+              <img className="TruckAvatar" src={this.props.truck.image} alt="Truck Image" />
               <div>
-                <h4 className='TruckName'>{this.props.truck.name}</h4>
-                <h6 className='TruckHandle'>{this.props.truck.handle}</h6>
+                <h4 className="TruckName">{this.props.truck.name}</h4>
+                <h6 className="TruckHandle">{this.props.truck.handle}</h6>
               </div>
-              <p className='TruckDescription'>{this.props.truck.description}</p>
-              <h5 className='TruckTweet'> {this.props.truck.message}</h5>
-              <div className='TruckTimeStamp'>{this.props.truck.timeStamp}</div>
+              <p className="TruckDescription">{this.props.truck.description}</p>
+              <h5 className="TruckTweet"> {this.props.truck.message}</h5>
+              <div className="TruckTimeStamp">{this.props.truck.timeStamp}</div>
           </div>
       </div>
       );
