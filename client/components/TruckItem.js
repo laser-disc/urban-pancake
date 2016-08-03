29fa0c39a45ae72395cce852d1ab655f6ec97070
George <georgejweiler@gmail.com>
2016-08-02 19:03:02 -0700

    style[client] finish MVP styling

--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -2,19 +2,24 @@
 
 import React, {Component} from 'react';
 
-
 // <FIX> ADD NAME AND DESCRIPTION FROM TWITTER
-export default class TruckItem extends Component {
-  render() {
-    return (
-      <div className='TruckItem'>Your Truck:
-        <div className='TruckName'>{this.props.truck.name}</div>
-        <div className='TruckHandle'>{this.props.truck.handle}</div>
-        <div className='TruckDescription'>{this.props.truck.description}</div>
-        <div className='TruckTweet'>Last Tweet: {this.props.truck.message}</div>
-        <div className='TruckTimeStamp'>Time: {this.props.truck.timeStamp}</div>
-        <img className='TruckAvatar' src={this.props.truck.image} />
+ export default class TruckItem extends Component {
+    
+    render() {
+      return (
+      <div className='TruckItem'>
+          <div className="tweet col-lg-offset-3 col-sm-6 col-md-6 col-lg-6">
+              <img className='TruckAvatar' src={this.props.truck.image} alt="Truck Image" />
+              <div>
+                <h4 className='TruckName'>{this.props.truck.name}</h4>
+                <h6 className='TruckHandle'>{this.props.truck.handle}</h6>
+              </div>
+              <p className='TruckDescription'>{this.props.truck.description}</p>
+              <h5 className='TruckTweet'> {this.props.truck.message}</h5>
+              <div className='TruckTimeStamp'>{this.props.truck.timeStamp}</div>
+          </div>
       </div>
-    );
-  };
-};
+      );
+    };
+ };
+
