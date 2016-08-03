c03f6ab8d3f75fa054334a94549a3e256002b92b
Reed Cureton <emailreedc@gmail.com>
2016-08-03 15:25:13 -0700

    Feat [client] Add unique marker with static location on map

--- client/components/GoogleMap.js
+++ client/components/GoogleMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap} from 'react-google-maps';
+import {GoogleMapLoader, GoogleMap, Marker, Size} from 'react-google-maps';
 
 
 
@@ -8,7 +8,10 @@ export default function map() {
     <GoogleMapLoader
       containerElement={ <div style={{height: '100%', width: '100%'}} /> }
       googleMapElement={
-        <GoogleMap defaultZoom={14} defaultCenter={{lat: 37.7874707, lng: -122.4019886}} />
+        <GoogleMap defaultZoom={14} defaultCenter={{lat: 37.7874707, lng: -122.4019886}}>
+          <Marker position={{lat: 37.7874707, lng: -122.4019886}}
+            label= 'HELLO WORLD' icon={{url:'https://pbs.twimg.com/profile_images/664169149002874880/z1fmxo00.jpg'}}/>
+        </GoogleMap>
       }
     />
   );
