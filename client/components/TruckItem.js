f6d2f518125720ec714d606888b9573040d11b55
George <georgejweiler@gmail.com>
2016-08-02 19:17:11 -0700

    fix[client] update imageUrl prop name

--- client/components/TruckItem.js
+++ client/components/TruckItem.js
@@ -9,7 +9,7 @@ import React, {Component} from 'react';
       return (
       <div className="TruckItem">
           <div className="tweet col-lg-offset-3 col-sm-6 col-md-6 col-lg-6">
-              <img className="TruckAvatar" src={this.props.truck.image} alt="Truck Image" />
+              <img className="TruckAvatar" src={this.props.truck.imageUrl} alt="Truck Image" />
               <div>
                 <h4 className="TruckName">{this.props.truck.name}</h4>
                 <h6 className="TruckHandle">{this.props.truck.handle}</h6>
