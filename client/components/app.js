bfa62b3f0e68b9a51e42bc34383e3c8802ceda20
George <georgejweiler@gmail.com>
2016-08-02 19:12:11 -0700

    refactor[client] fix quotes

--- client/components/app.js
+++ client/components/app.js
@@ -4,7 +4,7 @@ import TruckList from '../containers/TruckList';
 export default class App extends Component {
   render() {
     return (
-      <div className='container theme-light'>
+      <div className="container theme-light">
         <div className="jumbotron">
           <h1>Food Trucks Near You</h1>
           <p>...assuming you live in San Francisco </p>
