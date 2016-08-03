e0c9a79a6e374c89c4ada9d2e279dfa3aceaecfe
Blake Contreras <shardsofsilver@gmail.com>
2016-08-02 19:09:16 -0700

    test[init] Add FetchTrucks spec

---
+++ test/actions/FetchTrucks.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+import { expect } from 'chai';
+import { FETCH_TRUCKS, FetchTrucks } from '../../client/actions/FetchTrucks';
+
+describe('FetchTrucks action creator', () => {
+  it('should return an action with a type', () => {
+
+  })
+  it('should return an action with an object payload', () => {
+    
+  })
+});
