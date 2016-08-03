176b29bb4ac0240fcd235e138e1acc668ed3fa4f
Blake Contreras <shardsofsilver@gmail.com>
2016-08-02 19:22:32 -0700

    test[client] Add tests for FetchTrucks action creator.

--- test/actions/FetchTrucks.spec.js
+++ test/actions/FetchTrucks.spec.js
@@ -5,9 +5,10 @@ import { FETCH_TRUCKS, FetchTrucks } from '../../client/actions/FetchTrucks';
 
 describe('FetchTrucks action creator', () => {
   it('should return an action with a type', () => {
-
+    expect(FetchTrucks()).to.have.property('type');
   })
-  it('should return an action with an object payload', () => {
-    
+  it('should return an action with a payload', () => {
+    let payload = FetchTrucks().payload;
+    expect(payload).to.exist;
   })
 });
