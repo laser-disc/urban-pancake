ff59ec938069176cfa4ecb5e83ebe0fc0af8ecb7
George <georgejweiler@gmail.com>
2016-08-04 11:20:14 -0700

    feat[server] improve geocoder function to accept point of itnerest as an input

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -1,18 +1,38 @@
 
 const axios = require('axios');
 
-module.exports.geocoder = function(intersection){
-  return axios.get("/API/geocoder", {
-    params: {
-      intersection: intersection
-    }
-  })
-  .then(function(response){
-    return response.data
-  })
-  .catch(function(error){
-    if(error){
-      console.log(error)
-    }
-  }); 
+module.exports = function(locQuery){
+  if(locQuery.poi){
+    return axios.get("/API/poi", {
+      params: {
+        poi: locQuery.poi
+      }
+    })
+    .then(function(response){
+      console.log(response.data.name)
+      console.log(response.data.geometry.location)
+      return response.data
+    })
+    .catch(function(error){
+      if(error){
+        console.log(error)
+      }
+    }); 
+  }
+  if(locQuery.address){
+    return axios.get("/API/address", {
+      params: {
+        intersection: locQuery.address
+      }
+    })
+    .then(function(response){
+      console.log(response.data)
+      return response.data
+    })
+    .catch(function(error){
+      if(error){
+        console.log(error)
+      }
+    }); 
+  }
 };
