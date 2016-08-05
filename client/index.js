9ba707996cd37e26e717632494805a6c1bf4140f
Blake Contreras <shardsofsilver@gmail.com>
2016-08-05 15:24:24 -0700

    refactor[client] Refactor jsx code out of index.js and into app.jsx.

--- client/index.js
+++ client/index.js
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise';
-import App from './components/app';
+import App from './components/app.jsx';
 import rootReducer from './reducers/reducers';
 import './styles/normalize.css';
 import './styles/skeleton.css';
 import './styles/style.css';
 import 'babel-polyfill';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
-export default class Index extends Component {
-  render() {
-    return (
-      <Provider store={createStoreWithMiddleware(rootReducer)}>
-        <App />
-      </Provider>
-    )
-  }
-}
-
-ReactDOM.render(<Index />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
 
 // The entry point of our app must accept hot reloading in dev environment
 if (module.hot) {
