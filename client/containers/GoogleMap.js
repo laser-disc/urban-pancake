2c08aa7220ecab4012371c3fe8c07fd3ff9cf070
George <georgejweiler@gmail.com>
2016-08-05 10:40:28 -0700

    refactor[client] rewrite map code to work with googlemap-react dependency

--- client/containers/GoogleMap.js
+++ client/containers/GoogleMap.js
@@ -1,30 +1,57 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import {GoogleApiComponent} from 'google-maps-react';
+let secretKeys = null;
+if(!process.env['MONGOOSE_URI']) {
+  secretKeys = require('../env/config');
+}
+
+
+
+// import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
 // import Marker from '../components/Marker';
+// class Map extends Component {
+//   renderMarkers(truck) {
+//     return <Marker key={truck._id} position={truck.location} />
+//   };
+//   render() {
+//     return (
+//       <GoogleMapLoader
+//         containerElement={ <div style={{height: '100%', width: '100%'}} /> }
+//         googleMapElement={
+//           <GoogleMap defaultZoom={14} defaultCenter={{lat: 37.7874707, lng: -122.4019886}}>
+//             { this.props.trucks.map(truck => this.renderMarkers(truck)) }
+//           </GoogleMap>
+//         }
+//       />
+//     );
+//   }
+// };
+// function mapStateToProps(state) {
+//   return {
+//     trucks: state.trucks
+//   };
+// };
+// export default connect(mapStateToProps)(Map);
 
-class Map extends Component {
-  renderMarkers(truck) {
-    return <Marker key={truck._id} position={truck.location} />
-  };
-  render() {
+
+
+
+//NEED SWIRLY LOADING GIF
+class Container extends Component {
+  render(){
+    if(!this.props.loaded){
+      return (
+        <div> Loading... </div>
+      )
+    }
     return (
-      <GoogleMapLoader
-        containerElement={ <div style={{height: '100%', width: '100%'}} /> }
-        googleMapElement={
-          <GoogleMap defaultZoom={14} defaultCenter={{lat: 37.7874707, lng: -122.4019886}}>
-            { this.props.trucks.map(truck => this.renderMarkers(truck)) }
-          </GoogleMap>
-        }
-      />
-    );
+      <div>Map will go here</div>
+    )
   }
-};
+}
 
-function mapStateToProps(state) {
-  return {
-    trucks: state.trucks
-  };
-};
+export default GoogleApiComponent({
+  apiKey: secretKeys.GMAP_API_KEY;
+})(Container)
 
-export default connect(mapStateToProps)(Map);
