40e02f221c952f2ac012f9b43fa7741da15620f7
Matt Long <mlong635@gmail.com>
2016-08-05 18:08:50 -0700

    fix[server] no more duplication of incorrect data in db

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -5,17 +5,45 @@ if(!process.env['MONGOOSE_URI']) {
 }
 const GMAP_API_KEY = secretKeys.GMAP_API_KEY || process.env['GMAP_API_KEY'];
 
-module.exports = function(locQuery){
-  let query = locQuery.address || locQuery.poi;
-  let gMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query +"&key=" + GMAP_API_KEY;
-  return axios.get(gMapUrl)
-  .then(function(response){
-    console.log("Inside GeoCoder response.data.results", response.data.results);
-    return response.data.results[0].geometry.location;
-  })
-  .catch(function(error){
-    if(error){
-      console.log(error)
-    }
-  }); 
+////// old code before I started to refactor into a regular ES6 Promise
+module.exports = function(newTruckObj){
+
+  return new Promise((resolve, reject) => {
+    console.log("inside the GeoCoder, just received " + JSON.stringify(newTruckObj.getLocationResults));
+    let query = newTruckObj.getLocationResults.address || newTruckObj.getLocationResults.poi;
+    let gMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query +"&key=" + GMAP_API_KEY;
+    axios.get(gMapUrl)
+    .then(function(response){
+      console.log("Inside GeoCoder response.data.results", response.data.results[0].geometry.location);
+      newTruckObj.geoInfo = response.data.results[0].geometry.location;
+      resolve(newTruckObj);
+    })
+    .catch(function(error){
+      if(error){
+        console.log("Inside GeoCoder .catch(error)", error);
+        reject(error);
+      }
+    }); 
+  });
 };
+
+// module.exports = function(locQuery){
+//   return new Promise((resolve, reject) => {
+//     console.log("inside the GeoCoder, just received " + JSON.stringify(locQuery));
+//     let query = locQuery.address || locQuery.poi;
+//     let gMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query +"&key=" + GMAP_API_KEY;
+//     axios.get(gMapUrl)
+//     .then(function(response){
+//       console.log("Inside GeoCoder response.data.results", response.data.results);
+//       resolve(response.data.results[0].geometry.location);
+//     })
+//     .catch(function(error){
+//       if(error){
+//         console.log("Inside GeoCoder .catch(error)", error);
+//         reject(error);
+//       }
+//     }); 
+//   });
+// };
+
+
--- server/getLocationFromTweets.js
+++ server/getLocationFromTweets.js
@@ -4,35 +4,30 @@ let Address = addressValidator.Address;
 let geoCoder = require('../client/utils/utils')
 
 module.exports = {};
-module.exports.chosenIndex = undefined;
+// module.exports.chosenIndex = undefined;
 
-module.exports.getLocation = function (allTweets){
+module.exports.getLocation = function (newTruckObj){
+  let allTweets = newTruckObj.allTweetMessages;
   console.log("inside getLocation, just received "+allTweets.length+" tweets");
   return new Promise((resolve, reject) => {
-    let results = {poi: null, address: null};
-    let location, latLong, currentTweet, noPuncTweet;
+    let location, currentTweet, noPuncTweet;
     
     for(var i=0; i<allTweets.length; i++){
       currentTweet = (allTweets[i]);
-      console.log("inside the getLocation for loop current Tweet ", currentTweet);
-
       noPuncTweet = removePunctuation(currentTweet);
       location = atFromSubroutine(noPuncTweet);
       if(location){
-        module.exports.chosenIndex=i;
+        newTruckObj.chosenIndex = i;
+        newTruckObj.getLocationResults.address = location;
         break;
       }  
     }
-    results.address = location;
-    // resolve(results);
 
-    if (results.poi || results.address) {
-      console.log("getLocationFromTweets resolving results", results);
-      resolve(results)
+    if (newTruckObj.getLocationResults.poi || newTruckObj.getLocationResults.address) {
+      resolve(newTruckObj)
     } else {
-      console.log("getLocationFromTweets rejecting results", results);
-      reject(results);
-    };
+      reject(newTruckObj);
+    }
 
     function removePunctuation (strTweet) {
       // Removes the punctuation from the current tweet so that it's easier to parse
