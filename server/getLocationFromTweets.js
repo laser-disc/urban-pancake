0fcb59d5dcdca79d36e80190c80b30fd9b0be8a4
Matt Long <mlong635@gmail.com>
2016-08-06 16:55:03 -0700

    refactor[server] added intersection tweet parsing subroutine

--- server/getLocationFromTweets.js
+++ server/getLocationFromTweets.js
@@ -4,6 +4,11 @@ let Address = addressValidator.Address;
 
 module.exports = {};
 
+// list of street types according to https://data.sfgov.org/Geographic-Locations-and-Boundaries/Street-Names/6d9h-4u5v/data#column-menu
+let streetTypes = ['aly','ave','blvd','blvd north','blvd south','cir',
+  'ct','dr','expy','hl','hwy','ln','loop','park','path','pl','plz','ramp',
+ 'rd','row','st','stps','stwy','ter','tunl','walk','way'];
+
 // sample input: "We are serving Lunch at 1st st and Howard st from 11:15-1:45." output: "1st st and Howard st"  
 let atFromSubroutine = function(strTweet) {
   let arrTweet = strTweet.split(" ");
@@ -19,6 +24,33 @@ let atFromSubroutine = function(strTweet) {
   };
 };
 
+
+let intersection = function(strTweet) {
+  let words = strTweet.split(' and ');
+  if(words[0]===strTweet) return 'notFound';
+  let beforeStr = words[0].trim().split(' ');
+  let afterStr = words[1].trim().split(' ');
+  let firstStreet = beforeStr[beforeStr.length-1];
+  let secondStreet = afterStr[0];
+  for(let i=0; i<streetTypes.length; i++){
+    if(streetTypes[i]===firstStreet){
+      firstSteet = beforeStr[beforeStr.length-2] + ' '+ beforeStr[beforeStr.length-1];
+      break;
+    }
+  }
+  for(let i=0; i<streetTypes.length; i++){
+    if(streetTypes[i]===secondStreet){
+      secondSteet = afterStr[0] + ' '+ afterStr[1];
+      break;
+    }
+  }
+  // if(firstSteet ==="us" || firstStreet==="enter" || secondStreet==="us" || secondStreet==="enter"){
+  //   return 'notFound';
+  // }
+  return firstStreet && secondStreet ? (firstStreet + ' and ' + secondStreet) : 'notFound';
+};
+
+
 // this function looks for an exact address. input: "Our trucks will be at 225 Bush" output: "225 Bush" 
 let exactAddress = function (strTweet) {
   let wordAfterNum, punc, street, location, num;
@@ -33,7 +65,8 @@ let exactAddress = function (strTweet) {
     }
   }
   location = num + ' ' + street;
-  return location ? location : "notFound";
+  console.log("exactAddress", location);
+  return num && street ? location : "notFound";
 }
 
 module.exports.getLocation = function(newTruckObj) {
@@ -45,10 +78,14 @@ module.exports.getLocation = function(newTruckObj) {
     for(var i=0; i<allTweets.length; i++){
       currentTweet = (allTweets[i]);
       while(location===undefined){
+        //location = intersection(currentTweet);
         location = atFromSubroutine(currentTweet);
       }
       if(location==="notFound"){
         location = exactAddress(currentTweet);
+      }
+      if(location==="notFound"){
+        location = intersection(currentTweet);
       }
       // if location is notFound and we've gone through the entire array, reject the promise
       if(location ==="notFound" && i===allTweets.length-1){
@@ -57,7 +94,7 @@ module.exports.getLocation = function(newTruckObj) {
       }
       // if location is notFound but there are still tweets left in the array, reassign location to undefined then continue on to the next iteration
       if(location==="notFound") {
-        location=undefined;
+        location = undefined;
         continue;
       }
       // if location is found, save the chosenIndex and location in the newTruckObj and then resolve the promise
