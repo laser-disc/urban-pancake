c6c1218307ca2e9d6bf2bd452ef3e820b65fd64b
Matt Long <mlong635@gmail.com>
2016-08-06 08:54:08 -0700

    fix[git] merge conflicts

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,7 +5,7 @@ import {createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise';
 import rootReducer from '../reducers/reducers.js';
 import TruckList from '../containers/TruckList.jsx';
-import GoogleMap from '../containers/GoogleMap';
+import GoogleMap from '../containers/GoogleMap.jsx';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
