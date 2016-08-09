c0f0b016711ffbeeda9b7f6ec05bdf5fc7789797
George <georgejweiler@gmail.com>
2016-08-09 16:14:09 -0700

    feat[init/client] init truck view route - simple button on page now for testing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise';
 import rootReducer from '../reducers/reducers';
+import { Link } from "react-router";
+import {Router, Route, hashHistory} from 'react-router';
 import TruckList from '../containers/TruckList.jsx';
 import GoogleMap from '../containers/GoogleMap.jsx';
-
+import TruckView from '../containers/TruckView.jsx';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 const App = () => {
@@ -17,6 +19,7 @@ const App = () => {
           <h1>Food Trucks Near You</h1>
           <p>...assuming you live in San Francisco </p>
         </div>
+        <Link to="/truckview"><button>TRUCK VIEW</button></Link>
         <div style={{ height: '500px', width: '550px' }}>
           <GoogleMap />
         </div>
@@ -26,6 +29,11 @@ const App = () => {
   );
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path = "/" component={App} />
+    <Router path ="/truckview" component={TruckView} />
+  </Router>
+  , document.querySelector('#app'));
 
 export default App;
---
+++ client/containers/TruckView.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class TruckView extends Component {
+  render(){
+    return (
+      <h1>TRUCK VIEW</h1>
+    )
+  }
+}
+
+export default TruckView
