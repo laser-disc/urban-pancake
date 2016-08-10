42c8abed7f464910cbfa9258cd5cbd72261db585
bterrot <torre.bonnie@gmail.com>
2016-08-10 16:06:11 -0700

    fix[FetchYelp] Lint FetchYelp

--- client/actions/FetchYelp.js
+++ client/actions/FetchYelp.js
@@ -3,12 +3,8 @@ import axios from 'axios';
 export const FETCH_YELP = 'FETCH_YELP';
 
 export function FetchYelp() {
-  console.log('FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP FETCHING YELP')
   const url = '/API/yelp';
-  const request = axios.get(url,   {params: {
-      truckName: "Curry Up Now"
-    }
-  });
+  const request = axios.get(url, { params: { truckName: 'Curry Up Now' } });
   return {
     type: FETCH_YELP,
     payload: request,
