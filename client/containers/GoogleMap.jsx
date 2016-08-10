5e58d973c305d9fdd461aed026390a289a216467
Reed Cureton <emailreedc@gmail.com>
2016-08-10 12:14:34 -0700

    Feat[Client] Alter googlemap to grab proper location for marker from either twitter or DB schedule

--- client/containers/GoogleMap.jsx
+++ client/containers/GoogleMap.jsx
@@ -11,12 +11,11 @@ class Map extends Component {
     const date = new Date;
     const index = date.getDay();
     const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const tweetDay = truck.timeStamp.slice(0, 4);
+    const tweetDay = truck.timeStamp.slice(0, 3);
     const tweetIndex = dayOfWeek.indexOf(tweetDay);
-    console.log(dayOfWeek[index] === tweetDay);
-    console.log('OVER HERE',tweetDay, tweetIndex, dayOfWeek[2])
-    const position = truck.schedule[index].closed ? {lat: null, lng: null} : truck.schedule[index];
-    console.log('INSIDE GOOGLEMAP.JSX',index, position);
+
+    const position = tweetIndex === index ? truck.location : truck.schedule[index].closed ? {lat: null, lng: null} : truck.schedule[index];
+    console.log('INSIDE GOOGLEMAP.JSX',truck.name, position);
     return <Marker
       key={truck._id} position={{lat: position.lat, lng: position.lng}}
       // icon={{url:'https://media.giphy.com/media/8K1IYSnhUaNH2/giphy_s.gif'}}
