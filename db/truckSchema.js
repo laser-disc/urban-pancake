2e13f5bb35e5bb2155755ebfb0edf1905720c94b
bterrot <torre.bonnie@gmail.com>
2016-08-10 16:43:53 -0700

    fix[truckSchema] Lint truckSchema.js

--- db/truckSchema.js
+++ db/truckSchema.js
@@ -8,15 +8,15 @@ const truckSchema = new mongoose.Schema({
   timeStamp: String,
   imageUrl: String,
   yelpId: String,
-  location: {lat: Number, lng: Number},
+  location: { lat: Number, lng: Number },
   schedule: [
-    /*sunday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*monday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*tuesday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*wednesday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*thursday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*friday:*/ {lat: Number, lng: Number, closed: Boolean},
-    /*saturday:*/ {lat: Number, lng: Number, closed: Boolean},
+    /* sunday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* monday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* tuesday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* wednesday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* thursday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* friday:*/ { lat: Number, lng: Number, closed: Boolean },
+    /* saturday:*/ { lat: Number, lng: Number, closed: Boolean },
   ],
 });
 
