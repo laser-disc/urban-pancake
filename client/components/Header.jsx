ceaa75376a4b436a951ebe1814659cf8e1ce2a41
George <georgejweiler@gmail.com>
2016-08-11 19:54:02 -0700

    style[home] :art:

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,11 +4,14 @@ import {Link} from "react-router";
 export default class HomeHeader extends Component {
   render() {
     return ( 
-      <header className="">
-        <nav role="navigation" className="navbar navbar-default">
-          <div className="home-text"><Link to="/">Home</Link></div>
-          <div className="github">
-            <a href="https://github.com/laser-disc/urban-pancake" className="signin-button login">Check out the code</a>
+      <header className="row">
+        <nav className="navbar navbar-default">
+          <div className="home-text col-xs-offset-1 col-md-1"><Link to="/">Home</Link></div>
+          <div className ="col-xs-offset-1 col-md-offset-2 col-md-4">
+            <h1 >SF CITY FOOD TRUCKS</h1>
+          </div>
+          <div className="col-xs-offset-1 col-md-offset-2 col-md-2 github">
+            <a href="https://github.com/laser-disc/urban-pancake">Check out the code</a>
           </div>
          </nav>
        </header>
