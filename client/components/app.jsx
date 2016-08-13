adc191e1b30099ff2b0e9a81527ddb18875c0e3a
George <georgejweiler@gmail.com>
2016-08-13 15:43:51 -0700

    style home page :art:

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,6 +7,17 @@ import createLogger from 'redux-logger';
 import rootReducer from '../reducers/reducers';
 import {Link} from "react-router";
 import Router from '../components/Router.jsx';
+window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],  t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      t._e = []; t.ready = function(f) {
+        t._e.push(f);
+      };
+      return t;
+    }(document, "script", "twitter-wjs"));
 
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore);
