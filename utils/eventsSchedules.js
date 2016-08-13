411783cb2f36af0be8749ad9940bf57731a51651
bterrot <torre.bonnie@gmail.com>
2016-08-13 15:32:05 -0700

    chore[utils] Move eventsSchedules file from server to utils

---
+++ utils/eventsSchedules.js
@@ -0,0 +1,57 @@
+module.exports = {
+  gloungeSF: [
+    // location: lat: 37.7837031, lng: -122.394377
+    /* sunday: */ { closed: true },
+    /* monday: */ { closed: false },
+    /* tuesday: */ { closed: false },
+    /* wednesday: */ { closed: false },
+    /* thursday: */ { closed: false },
+    /* friday: */ { closed: false },
+    /* saturday: */ { closed: true },
+  ],
+  // 'Off the Grid': [
+  //   // locations vary
+  //   /* sunday: */ { closed: true },
+  //   /* monday: */ { closed: false },
+  //   /* tuesday: */ { closed: false },
+  //   /* wednesday: */ { closed: false },
+  //   /* thursday: */ { closed: false },
+  //   /* friday: */ { closed: false },
+  //   /* saturday: */ { closed: true },
+    // /* sunday: */ { lat: 37.800739, lng: -122.458237, closed: false },
+    // /* monday: */ { lat: 37.775640, lng: -122.39756, closed: false },
+    // /* tuesday: */ { lat: 37.779869, lng: -122.413745, closed: false },
+    // /* wednesday: */ { lat: 37.782115, lng: -122.406234, closed: false },
+    // /* thursday: */ { lat: 37.779869, lng: -122.413745, closed: false },
+    // /* friday: */ { lat: 37.779379, lng: -122.418401, closed: false },
+    // /* saturday: */ { lat: 37.727870, lng: -122.477082, closed: false },
+  SPARKsocialSF: [
+    //location = lat: 37.770798, lng: -122.391602
+    /* sunday: */ { closed: false },
+    /* monday: */ { closed: false },
+    /* tuesday: */ { closed: false },
+    /* wednesday: */ { closed: false },
+    /* thursday: */ { closed: false },
+    /* friday: */ { closed: false },
+    /* saturday: */ { closed: false },
+  ],
+  SoMaStrEatFood: [
+    //location = 37.772640, lng: -122.409915
+    /* sunday: */ { closed: false },
+    /* monday: */ { closed: false },
+    /* tuesday: */ { closed: false },
+    /* wednesday: */ { closed: false },
+    /* thursday: */ { closed: false },
+    /* friday: */ { closed: false },
+    /* saturday: */ { closed: false },
+  ],
+  // 'Truck Stop SF': [
+  //   //location = 37.790135, lng: -122.397464
+  //   /* sunday: */ { closed: true },
+  //   /* monday: */ { closed: false },
+  //   /* tuesday: */ { closed: false },
+  //   /* wednesday: */ { closed: false },
+  //   /* thursday: */ { closed: false },
+  //   /* friday: */ { closed: false },
+  //   /* saturday: */ { closed: true },
+};
