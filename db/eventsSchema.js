974d1b7704f98c360a69a554720c16fc58dae2e5
bterrot <torre.bonnie@gmail.com>
2016-08-13 15:29:34 -0700

    feat[db] Add food truck events collection and schema to the database

---
+++ db/eventsSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const eventsSchema = new mongoose.Schema({
+  name: String,
+  handle: String,
+  description: String,
+  message: String,
+  timeStamp: String,
+  imageUrl: String,
+  location: { lat: Number, lng: Number },
+  schedule: [
+    /* sunday: */ { closed: Boolean },
+    /* monday: */ { closed: Boolean },
+    /* tuesday: */ { closed: Boolean },
+    /* wednesday: */ { closed: Boolean },
+    /* thursday: */ { closed: Boolean },
+    /* friday: */ { closed: Boolean },
+    /* saturday: */ { closed: Boolean },
+  ],
+  // currentTrucks: {
+  //   today: Number,
+  //   trucksArray: Array,
+  // },
+  // yelpId: String,
+  // yelpInfo: {
+  //   name: String,
+  //   yelpBizID: String,
+  //   starsRating: String,
+  //   review_count: Number,
+  //   custReview: String,
+  //   photo: String,
+  //   categories: String,  // aka 'cuisine'
+  // }
+});
+
+const Event = mongoose.model('Food Truck Events', eventsSchema);
+
+module.exports = Event;
--- server/request-handler.js
+++ server/request-handler.js
@@ -6,16 +6,19 @@
 
 const db = require('../db/config');
 const mongoose = require('mongoose');
-const https = require('https');
+const https = require('https')
 const Truck = require('../db/truckSchema');
+const Event = require('../db/eventsSchema');
 const { getLocation } = require('./getLocationFromTweets');
 const { createTruckWithGeoInfo } = require('./updateTruckInfo');
 const { getTruckTwitterInfo } = require('./updateTruckInfo');
+const { createEventRecord, getEventTwitterInfo, createOrUpdateEvent } = require('./updateEventInfo');
 const { createOrUpdateDB } = require('./updateTruckInfo');
 const { geoCoder } = require('../utils/utils');
 const { getYelpInfo } = require('./updateTruckInfo');
 const { getFiveTweets } = require('./updateTruckInfo');
 const { updateDBwithYelpInfo } = require('./updateTruckInfo');
+
 
 // make sure to add the exact Twitter handle minus the @
 const foodTrucks = ['JapaCurry', 'CurryUpNow', 'chairmantruck', 'slidershacksf', 'KokioRepublic'];
@@ -30,6 +33,34 @@ const foodTrucksObj = {
   slidershacksf: { twitterHandle: 'slidershacksf', yelpBizID: 'slider-shack-san-francisco'},
   KokioRepublic: { twitterHandle: 'KokioRepublic', yelpBizID: 'kokio-republic-san-francisco'},
 };
+
+
+
+
+
+
+
+// iterates over an array of food truck event Twitter handles
+foodEvents.forEach(event => {
+  return getEventTwitterInfo
+  // from the most recent tweet, we compile the info for our DB record
+  .then(newEvent => createEventRecord(newEvent))
+  // either adds the info from createEventRecord to the DB or updates this existing info
+  .then(newEvent => createOrUpdateEvent(newEvent))
+  .catch(err => res.status(400).send(err));  
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 foodTrucks.forEach(foodTruck => {
   return getTruckTwitterInfo(foodTruck)
@@ -46,34 +77,32 @@ foodTrucks.forEach(foodTruck => {
 
 
 module.exports = (app) => {
-
+  
   app.get('/API/fetchAll', (req, res) => {
     Truck.find((err, trucks) => res.status(200).send(trucks));
   });
+  app.get('/API/fetchAll', (req, res) => {
+    Event.find((err, allEvents) => res.status(200).send(allEvents));
+  });
+
   app.get('/API/fetch', (req, res) => {
     // handle must be different for test and client
     const handle = req.body.params ? req.body.params.handle : req.query.handle;
     Truck.findOne({ handle }, (err, truck) => res.status(200).send(truck));
   });
-  app.get('/API/yelp', (req,res) => {
+
+  app.get("/API/yelp", (req,res) => {
     let handle = '@' + req.query.truckName;
     Truck.findOne({ handle }, (err, truck) => res.status(200).send(truck))
     .catch( err => res.status(400).send(err))
   });
   app.get("/API/fiveTweets", (req,res) => {
-    getTruckTwitterInfo(req.query.truckName)
-    .then( newTruckObj => {
-      newTruckObj.fiveTweetObjs = [];
-      return getFiveTweets(newTruckObj, newTruckObj.allTweetObjs[0].id_str)
+    let truck = {};
+    truck.truckName = req.query.truckName;
+    return getFiveTweets(truck)
+    .then(truckInfo => {
+      res.status(200).send(truckInfo)
     })
-    .then( newTruckObj => getFiveTweets(newTruckObj, newTruckObj.allTweetObjs[1].id_str))
-    .then( newTruckObj => getFiveTweets(newTruckObj, newTruckObj.allTweetObjs[2].id_str))
-    .then( newTruckObj => getFiveTweets(newTruckObj, newTruckObj.allTweetObjs[3].id_str))
-    .then( newTruckObj => getFiveTweets(newTruckObj, newTruckObj.allTweetObjs[4].id_str))
-    .then(truckInfo => res.status(200).send(truckInfo))
-    .catch(err => {
-      console.log("request-handler API/fiveTweets unsuccessful");
-      res.status(400).send(err);
-    })
+    .catch(err => res.status(400).send(err));
   });
 };
