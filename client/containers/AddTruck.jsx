7ea773bd59666f13eb7ba2db83dafd274daa83a4
Blake Contreras <shardsofsilver@gmail.com>
2016-08-15 17:16:48 -0700

    feat[add truck] Implement change detection for add truck form.

--- client/containers/AddTruck.jsx
+++ client/containers/AddTruck.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router'
 import { bindActionCreators } from 'redux';
 
 class AddTruck extends Component {
+  // initial state: empty truck object
+    // populate with initial state?
   // form
     // fields:
       // name*  - str
@@ -20,22 +22,42 @@ class AddTruck extends Component {
     // create Promise, send to server
     // server says OK, create action to update state
     // post success message
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      truck: {
+        name: '',
+        handle: ''
+      }
+    };
+    this.getValue = this.getValue.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   handleSubmit(event) {
     console.log('event: ', event, ' target: ', event.target, ' this: ', this)
   }
-
+  handleChange(event) {
+    const newState = this.state;
+    newState.truck[event.target.name] = event.target.value;
+    this.setState(newState);
+    console.log(this.getValue());
+  }
+  getValue() {
+    return this.state.truck;
+  }
     // TODO: Add help option to describe Yelp ID
   render() {
     return (
       <div>
         <form
           className="add-truck"
-          onSubmit={this.handleSubmit}>
+          onChange={this.handleChange}>
           <label>Name</label>
           <input
             type="text"
             placeholder="name"
+            name="name"
             />
           <label>Twitter Handle</label>
           @<input
