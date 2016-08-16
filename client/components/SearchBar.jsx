900da28c3abf9fe047d350f0f8b17e0367b30ef0
bterrot <torre.bonnie@gmail.com>
2016-08-16 11:42:10 -0700

    feat[client] Create search bar component with basic styling

---
+++ client/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return <input className="search-bar" placeholder="Search for your favorite truck or food here!" />
+};
+
+export default SearchBar;
