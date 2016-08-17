0b0f76712891c94e272312d6344f5b1e0873b54b
bterrot <torre.bonnie@gmail.com>
2016-08-17 13:23:53 -0700

    fix[reducer] Change to TruckList reducer so that only trucks that have tweeted a location today appear in list

--- client/reducers/reducer_TruckList.js
+++ client/reducers/reducer_TruckList.js
@@ -4,7 +4,16 @@ import { FETCH_TRUCKS } from '../actions/FetchTrucks';
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_TRUCKS:
-      return [...action.payload.data];
+      const everyTruck = action.payload.data;
+      const today = (new Date()).toString().slice(0, 10); //should be something like 'Wed'
+      return everyTruck.filter((truck) => {
+        const lastTweetDay = truck.timeStamp.slice(0, 10);
+        if (today !== lastTweetDay) {
+          console.log('Throwing out', truck.name);
+        } else {
+          return truck;
+        }
+      });
     default:
       return state;
   }
