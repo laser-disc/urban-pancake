0932fc0e5aa812ad4bbfb3d4c9a461d78edafce5
bterrot <torre.bonnie@gmail.com>
2016-08-18 10:19:09 -0700

    refactor[searchBar] Refactor to console.log user input
    This feature is NOT finished

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input className="search-bar" placeholder="Search for your favorite truck or food here!" />
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: '' };
+  }
+
+  render() {
+    return (
+      <input
+        className="search-bar"
+        placeholder="Search for your favorite truck or food here!"
+        onChange={event => console.log(event.target.value)}
+      />
+    );
+  };
 };
 
 export default SearchBar;
