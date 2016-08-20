ebc7e350675c1a566b216c2e8b0e7ab13d031684
Reed Cureton <emailreedc@gmail.com>
2016-08-19 19:37:21 -0700

    Refactor[Server] code cleanup utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -7,12 +7,13 @@ if(!process.env.MONGOOSE_URI) {
 const GMAP_API_KEY = secretKeys ? secretKeys.GMAP_API_KEY : process.env.GMAP_API_KEY;
 
 module.exports = {};
-module.exports.geoCoder = (newTruckObj, day) => {
+module.exports.geoCoder = (newTruckObj) => {
   return new Promise(function(resolve, reject) {
     let query = newTruckObj.getLocationResults.address || newTruckObj.getLocationResults.poi;
     let gMapUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GMAP_API_KEY}`;
     axios.get(gMapUrl)
     .then(function(response) {
+      console.log(response.data);
       newTruckObj.geoInfo = response.data.results[0].geometry.location;
       resolve(newTruckObj);
     })
@@ -23,6 +24,26 @@ module.exports.geoCoder = (newTruckObj, day) => {
     });
   });
 };
+
+// module.exports.geoCoder = (newTruckObj) => {
+//   return new Promise(function(resolve, reject) {
+//     let query = newTruckObj.getLocationResults.address || newTruckObj.getLocationResults.poi;
+//     let gMapUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GMAP_API_KEY}`;
+//     axios.get(gMapUrl)
+//     .then(function(response) {
+//       newTruckObj.geoInfo = response.data.results[0].geometry.location;
+//       resolve(newTruckObj);
+//     })
+//     .catch(function(error) {
+//       if(error){
+//         reject(error);
+//       }
+//     });
+//   });
+// };
+
+
+
 
 module.exports.newTruckGeoCoder = (newTruckObj, dayLocation) => {
   return new Promise(function(resolve, reject) {
@@ -42,7 +63,7 @@ module.exports.newTruckGeoCoder = (newTruckObj, dayLocation) => {
     .catch(function(error) {
       if(error){
         console.log("newTruckGeoCoder error ", error);
-        reject(error); 
+        reject(error);
       }
     });
   });
